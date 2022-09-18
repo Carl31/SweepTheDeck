7dc9536e91c41aa7aaa34555a668e2851a6051ad
alphaaa77 <hughmagallanes77@gmail.com>
2022-09-18 21:38:04 +1200

    more movement, final changes

--- SweepTheDeck/Assets/Main Character/PlayerMovement.cs
+++ SweepTheDeck/Assets/Main Character/PlayerMovement.cs
@@ -20,9 +20,42 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
+    public void MoveLeft()
+    {
+        moveLeft = true;
+    }
+
+    public void MoveRight()
+    {
+        moveRight = true;
+    }
+
+    public void Jump()
+    {
+        if (rb.velocity.y == 0)
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    public void StopMoving()
+    {
+        moveLeft = false;
+        moveRight = false;
+        rb.velocity = Vector2.zero;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (moveLeft)
+        {
+            rb.velocity = new Vector2(-moveSpeed, 0f);
+        }
+
+        if (moveRight)
+        {
+            rb.velocity = new Vector2(moveSpeed, 0f);
+        }
     }
 }
