9086cf025ffba63ce7b5d0f00c7c60f9f386ad52
janenotjung-hue <107402425+janenotjung-hue@users.noreply.github.com>
2022-10-06 15:01:15 +1300

    set music for different scenes
    music can now be changed whenever there is a new scene loaded

--- SweepTheDeck/Assets/Scenes/Scripts/AudioManager.cs
+++ SweepTheDeck/Assets/Scenes/Scripts/AudioManager.cs
@@ -2,38 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
     [SerializeField] AudioMixer mixer;
-    [SerializeField] AudioSource eatSource;
-    [SerializeField] List<AudioClip> eatClips = new List<AudioClip>();
+    [SerializeField] AudioSource audioSource; //bgm source
+
+    public AudioClip[] bgmClips;
 
     public const string MUSIC_KEY = "musicVolume";
     public const string SOUND_KEY = "soundVolume";
 
-
     void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
-        } else
+        }
+        else
         {
             Destroy(gameObject);
         }
 
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
         LoadVolume();
     }
-
-    public void AttackSFX()
+    //called whenever new scene is loaded
+    void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
     {
-        AudioClip clip = eatClips[Random.Range(0, eatClips.Count)];
+        AudioClip source;
 
-        eatSource.PlayOneShot(clip);
+        switch (scene.buildIndex)
+        {
+            case 1:
+                Debug.Log(1);
+                source = bgmClips[1];
+                break;
+            case 2:
+                Debug.Log(2);
+                source = bgmClips[2];
+                break;
+            default:
+                Debug.Log("0 or 3");
+                source = bgmClips[0];
+                break;
+        }
+        //this command at the moment will give a nullreference error when switching between scenes as
+        //we're amending audioSource which already got deleted at the start
+        if(source != audioSource.clip) 
+        {
+            Debug.Log("doesnt match");
+            audioSource.enabled = false;
+            audioSource.clip = source;
+            audioSource.enabled = true;
+        }
     }
+
+    /*LIST OF PLAYER SFX
+     *WALK
+     *JUMP
+     *LAND AFTER JUMP
+     *ATTACK- VOICE
+     *ATTACK- SLASH
+     *ATTACK- GUN
+     *TAKE DAMAGE- VOICE
+     *TAKE DAMAGE- HIT
+     *DIE
+     *COLLECT COINS (maybe)
+     
+    public void AttackSFX() //LOL
+    {
+        AudioClip clip = playerAudioClips[Random.Range(0, playerAudioClips.Count)];
+
+        playerAudioSource.PlayOneShot(clip);
+    }*/
 
     void LoadVolume() //volume saved in VolumeSettings.cs
     {
@@ -41,10 +86,11 @@ public class AudioManager : MonoBehaviour
         float sound = PlayerPrefs.GetFloat(SOUND_KEY, 1f);
         int musicMute = PlayerPrefs.GetInt(VolumeSettings.MUSIC_MUTE, 0);
         int soundMute = PlayerPrefs.GetInt(VolumeSettings.SOUND_MUTE, 0);
-        if(musicMute == 1)
+        if (musicMute == 1)
         {
             mixer.SetFloat(VolumeSettings.MIXER_MUSIC, Mathf.Log10(0.0001f) * 20);
-        } else if(musicMute == 0)
+        }
+        else if (musicMute == 0)
         {
             mixer.SetFloat(VolumeSettings.MIXER_MUSIC, Mathf.Log10(music) * 20);
         }
--- SweepTheDeck/Assets/Scenes/Scripts/SwitchScene.cs
+++ SweepTheDeck/Assets/Scenes/Scripts/SwitchScene.cs
@@ -5,38 +5,63 @@ using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour
 {
-    public void BackButton()
+    /*INDEX
+     * 0 = starting screen
+     * 1 = battle
+     * 2 = shanty
+     * 3 = settings
+     * 4 = leaderboard (DOESNT EXIST YET)
+     */
+
+    public const string PREV_PREV_SCENE = "prevPrevScene"; //dumb but we need this to get to the scene we came from when we use the back button
+    public const string PREV_SCENE = "prevScene"; //technically this IS the current scene
+    public const string CURRENT_SCENE = "currentScene"; //this would be next_scene in terms of order
+
+    private void Awake()
     {
-        SceneManager.LoadScene("StartingScreen");
+        int prevScene = SceneManager.GetActiveScene().buildIndex;
+        SetPrevScene(prevScene);
+    }
+    public void BackButton() //for settings + shanty
+    {
+        int returnSceneIndex = PlayerPrefs.GetInt(PREV_PREV_SCENE, 0);
+        SetCurrentScene(returnSceneIndex);
+        SceneManager.LoadScene(returnSceneIndex);
     }
 
     public void LoadMainMenu()
     {
+        SetCurrentScene(0); //SceneManager.GetSceneByName("StartingScreen").buildIndex;
         SceneManager.LoadScene("StartingScreen");
         Debug.Log("StartingScreen");
     }
 
     public void LoadInGame()
     {
+        SetCurrentScene(1); //SceneManager.GetSceneByName("Battle").buildIndex;
         SceneManager.LoadScene("Battle");
         Debug.Log("Battle");
     }
 
-    public void LoadLeaderboard()
+    public void LoadShanty()
     {
-        Debug.Log("LoadLeaderboard Clicked");
+        SetCurrentScene(2); //SceneManager.GetSceneByName("Shanty").buildIndex;
+        SceneManager.LoadScene("Shanty");
+        Debug.Log("Shanty");
     }
 
     public void LoadSettings()
     {
+        SetCurrentScene(3); //SceneManager.GetSceneByName("Settings").buildIndex;
         SceneManager.LoadScene("Settings");
         Debug.Log("Settings");
     }
 
-    public void LoadShanty()
+    public void LoadLeaderboard()
     {
-        SceneManager.LoadScene("Shanty");
-        Debug.Log("Shanty");
+        //SetPrevScene(SceneManager.GetActiveScene().buildIndex);
+        //SetCurrentScene(4); //SceneManager.GetSceneByName("Leaderboard").buildIndex;
+        Debug.Log("LoadLeaderboard Clicked");
     }
 
     public void QuitGame()
@@ -51,4 +76,19 @@ public class SwitchScene : MonoBehaviour
         Debug.Log("opening feedback");
     }
 
+    public void SetCurrentScene(int currentScene)
+    {
+        PlayerPrefs.SetInt(CURRENT_SCENE, currentScene);
+        PlayerPrefs.Save();
+    }
+    public void SetPrevScene(int prevScene)
+    {
+        PlayerPrefs.SetInt(PREV_SCENE, prevScene);
+        PlayerPrefs.Save();
+    }
+
+    public void SetPrevPrevScene(int prev2Scene)
+    {
+        PlayerPrefs.SetInt(PREV_PREV_SCENE, prev2Scene);
+    }
 }
