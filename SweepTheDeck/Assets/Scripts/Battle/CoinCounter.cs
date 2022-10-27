434a05bbc6feb49cf321099be99c519c635367ea
janenotjung-hue <107402425+janenotjung-hue@users.noreply.github.com>
2022-10-28 12:48:17 +1300

    Patch
    asdasdasd

--- SweepTheDeck/Assets/Scripts/Battle/CoinCounter.cs
+++ SweepTheDeck/Assets/Scripts/Battle/CoinCounter.cs
@@ -7,15 +7,19 @@ public class CoinCounter : MonoBehaviour
 {
     public static CoinCounter instance;
     public TextMeshProUGUI text;
-    int coinCount;
+    int coinCount = 0;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         if (instance == null)
         {
             instance = this;
+        } else
+        {
+            Destroy(instance);
         }
+        DontDestroyOnLoad(instance);
     }
 
     public void UpdateCount(int coins)
@@ -23,5 +27,10 @@ public class CoinCounter : MonoBehaviour
         Debug.Log("test");
         coinCount += coins;
         text.SetText(coinCount.ToString());
+    }
+
+    public int GetCoinCount()
+    {
+        return coinCount;
     }
 }
